039d2ea234ccf2faf883e24cb3d5171344a1fe92
Daniel Azulay <daniel@danielazulay.eu>
2021-06-05 01:13:04 +0200

    Parse args

--- neuroquant.py
+++ neuroquant.py
@@ -6,26 +6,21 @@ class NeuroQuant(object):
     def __init__(self, config_filename):
         self.config_filename = config_filename
 
-    def get_config(self):
-        return NQConfig(self.config_filename)
+    def get_server(self):
+        return NQServer(NQConfig(self.config_filename))
 
-    def start_server(self):
-        return lambda: NQServer(self.get_config())
-
-    def start_client(self):
-        return NQClient(self.get_config())
+    def get_client(self):
+        return NQClient(NQConfig(self.config_filename))
 
 
 parser = argparse.ArgumentParser(prog='neuroquant')
-parser.add_argument('-c', '--config')
+parser.add_argument('-c', '--config', default="etc/config.json")
 parser.add_argument('--daemon', action='store_true')
 
 args = parser.parse_args()
-print(args)
 
-
-
-
-
-
-
+nq = NeuroQuant(args.config)
+if args.daemon:
+    nq.get_server().start()
+else:
+    nq.get_client().start()
